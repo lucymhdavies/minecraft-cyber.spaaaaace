22822b2502e8544497b4cd84feb9a0e06caa381d
Lucy Davinhart <lucy@lucymhdavies.com>
2020-03-19 13:24:01 +0000

    Be more verbose in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Downloading Tarball"
+
 wget https://github.com/lucymhdavies/minecraft-cyber.spaaaaace/releases/download/2020-03-19.1200/map.tar.gz
+
+echo "Extracting"
 
 tar -zxvf map.tar.gz
