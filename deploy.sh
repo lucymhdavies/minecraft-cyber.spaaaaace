23a81e3a34632f34fd408d703f65c2bf7f41a475
Lucy Davinhart <lucy@lucymhdavies.com>
2020-03-19 13:26:49 +0000

    Download with curl instead

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 echo "Downloading Tarball"
 
-wget https://github.com/lucymhdavies/minecraft-cyber.spaaaaace/releases/download/2020-03-19.1200/map.tar.gz
+curl -L https://github.com/lucymhdavies/minecraft-cyber.spaaaaace/releases/download/2020-03-19.1200/map.tar.gz > map.tar.gz
 
 echo "Extracting"
 
