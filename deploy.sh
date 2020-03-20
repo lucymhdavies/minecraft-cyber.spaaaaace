f7c2be55ea9d2f77c408cf0498300adb90ecf844
Lucy Davinhart <lucy@lucymhdavies.com>
2020-03-20 00:02:13 +0000

    2020-03-19.2350 map files

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 echo "Downloading Tarball"
 
-tarball="https://github.com/lucymhdavies/minecraft-cyber.spaaaaace/releases/download/2020-03-19.2330/map.tar.gz"
+tarball="https://github.com/lucymhdavies/minecraft-cyber.spaaaaace/releases/download/2020-03-19.2350/map.tar.gz"
 
 curl -L ${tarball} > map.tar.gz
 
