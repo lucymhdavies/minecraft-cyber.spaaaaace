1be48271b18554001d3f10dffd690653dd44030f
Lucy Davinhart <lucy@lucymhdavies.com>
2020-03-26 10:57:51 +0000

    Latest map, based on world from 2020-03-26 0924

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 echo "Downloading Tarball"
 
-tarball_date="2020/03/24/1849"
+tarball_date="2020/03/26/0924"
 tarball="https://s3-eu-west-1.amazonaws.com/minecraft-cyber.spaaaaace/${tarball_date}/map.tar.gz"
 
 curl -s -L ${tarball} > map.tar.gz
